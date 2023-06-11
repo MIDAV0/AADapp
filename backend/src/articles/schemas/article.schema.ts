cebac976ed1abcac6d1224e1482f1093f992fb93
F Hersi <33755306+fhersi@users.noreply.github.com>
2023-06-11 12:11:41 +0100

    add article schema

---
+++ backend/src/articles/schemas/article.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ArticleDocument = Article & Document;
+
+@Schema()
+export class Article {
+  @Prop()
+  articleId: string;
+  @Prop()
+  title: string;
+  @Prop()
+  author: string;
+  @Prop()
+  body: string;
+}
+
+export const ArticleSchema = SchemaFactory.createForClass(Article);
