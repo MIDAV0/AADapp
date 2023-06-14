26232878aeca80b99e38d7b6f762912542a5ddc7
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-06-14 17:55:19 +0100

    Pass arguments

--- components/homePageBody.tsx
+++ components/homePageBody.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Link from 'next/link'
 import * as data from '../utils/articles.json'
 import ArticleCard from './ArticleCard'
-const PageBody = () => (
+const PageBody = ({ smartAccount, storeContract}) => (
     <div className="container mx-auto flex flex-wrap py-6">
             <div className="container my-12 mx-auto px-4 md:px-12">
                 <div className="flex flex-wrap -mx-1 lg:-mx-4">
                     {data.map((article) => {
                         return (
-                            <ArticleCard article={article} />
+                            <ArticleCard 
+                                article={article} 
+                                storeContract={storeContract}
+                                smartAccount={smartAccount}    
+                            />
                         )
                     })}
                 </div>
