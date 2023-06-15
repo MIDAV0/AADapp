195a8336d9ebe2e4b7da485fba8517663a1faee6
Vadzim Mahilny <magilnyy.vadimo@gmail.com>
2023-06-15 11:15:02 +0100

    Passed provider

--- components/homePageBody.tsx
+++ components/homePageBody.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 import * as data from '../utils/articles.json'
 import ArticleCard from './ArticleCard'
-const PageBody = ({ smartAccount, storeContract}) => (
+
+const PageBody = ({ smartAccount, storeContract, provider }) => (
     <div className="container mx-auto flex flex-wrap py-6">
             <div className="container my-12 mx-auto px-4 md:px-12">
                 <div className="flex flex-wrap -mx-1 lg:-mx-4">
@@ -11,7 +12,8 @@ const PageBody = ({ smartAccount, storeContract}) => (
                             <ArticleCard 
                                 article={article} 
                                 storeContract={storeContract}
-                                smartAccount={smartAccount}    
+                                smartAccount={smartAccount}
+                                provider={provider}    
                             />
                         )
                     })}
